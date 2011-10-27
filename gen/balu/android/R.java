790c8778b33e3a235aa2d1ad4114d7d6d066b32f
Bala Subrahmayam Varanasi <vabasu@gmail.com>
2011-10-27 23:06:52 +0530

    this final commit for todays work

--- gen/balu/android/R.java
+++ gen/balu/android/R.java
@@ -15,6 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int enter=0x7f050000;
+        public static final int radio0=0x7f050003;
+        public static final int radio1=0x7f050004;
+        public static final int radioGroup1=0x7f050002;
+        public static final int spinner1=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -22,6 +26,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int female=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int male=0x7f040003;
+        public static final int origin=0x7f040002;
     }
 }
