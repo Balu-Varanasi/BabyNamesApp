9f5b70808b5ea90aa0a529aa9ce596ada8df8a37
Bala Subrahmayam Varanasi <vabasu@gmail.com>
2011-11-06 01:11:45 +0530

    Added Common Names Activity

---
+++ src/balu/android/CommonNames.java
@@ -0,0 +1,31 @@
+package balu.android;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class CommonNames extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		CommonNameTable cnTable = new CommonNameTable(this);
+		cnTable.open();
+		Cursor c = cnTable.fetchAllCommonNames();
+		if (c != null){
+		SimpleCursorAdapter adapter2 = new SimpleCursorAdapter(this,
+		    R.layout.listview,
+		    c, // Give the cursor to the list adapter
+		  new String[] {c.getColumnName(1),c.getColumnName(2)}, // Map the column in the
+		    // employee database to...
+		new int[] {R.id.commonName, R.id.commonNameCount}); // The view defined in the XML template
+		    ListView cnListView = (ListView)findViewById(R.id.MainLayout);
+		    cnListView.setAdapter(adapter2);
+		    }
+		    cnTable.close();
+	}
+}
